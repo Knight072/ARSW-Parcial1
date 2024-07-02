b24486552446e49d14178b313b69d3be3e33b6ff
unknown <daniel.rojas-h@labinfo.is.escuelaing.edu.co>
2024-07-02 10:19:37 -0500

    primer commit

---
+++ src/main/java/edu/escuelaing/arsw/ExampleController.java
@@ -0,0 +1,23 @@
+package edu.escuelaing.arsw;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/example")
+    public String example() {
+        return "example";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
+
